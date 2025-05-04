a932e522eda110cdb581e056ba6c7421e7a246f6
Mina <TMDHammond@outlook.com>
2025-05-03 20:35:39 -0700

    Fix Cypress config, test setup, and UI data-testid issues for full E2E test pass

--- cypress/e2e/quiz.cy.js
+++ cypress/e2e/quiz.cy.js
@@ -1,32 +1,39 @@
+import '@testing-library/cypress/add-commands';
+
 describe('Tech Quiz Website', () => {
-    beforeEach(() => {
-      cy.visit('http://localhost:3000');
-    });
-  
-    it('should display the start button on load', () => {
-      cy.findByRole('button', { name: /start quiz/i }).should('be.visible');
-    });
-  
-    it('should start the quiz when the start button is clicked', () => {
-      cy.findByRole('button', { name: /start quiz/i }).click();
-      cy.findByTestId('question-text').should('be.visible');
-    });
-  
-    it('should complete the quiz and show the score after 10 questions', () => {
-      cy.findByRole('button', { name: /start quiz/i }).click();
-      for (let i = 0; i < 10; i++) {
-        cy.get('[data-testid="answer-button"]').first().click();
-      }
-      cy.findByTestId('score-text').should('be.visible');
-    });
-  
-    it('should allow starting a new quiz after finishing', () => {
-      cy.findByRole('button', { name: /start quiz/i }).click();
-      for (let i = 0; i < 10; i++) {
-        cy.get('[data-testid="answer-button"]').first().click();
-      }
-      cy.findByRole('button', { name: /start quiz/i }).click();
-      cy.findByTestId('question-text').should('be.visible');
-    });
+  beforeEach(() => {
+    cy.visit('http://localhost:3001');
   });
-  
+
+  it('should display the start button on load', () => {
+    cy.findByTestId('start-button').should('be.visible');
+  });
+
+  it('should start the quiz when the start button is clicked', () => {
+    cy.findByTestId('start-button').click();
+    cy.findByTestId('question-text', { timeout: 8000 }).should('be.visible');
+  });
+
+  it('should complete the quiz and show the score after 10 questions', () => {
+    cy.findByTestId('start-button').click();
+    cy.get('[data-testid="answer-button"]', { timeout: 8000 }).first().should('be.visible');
+
+    for (let i = 0; i < 10; i++) {
+      cy.get('[data-testid="answer-button"]').first().click();
+    }
+
+    cy.findByTestId('score-text', { timeout: 8000 }).should('be.visible');
+  });
+
+  it('should allow starting a new quiz after finishing', () => {
+    cy.findByTestId('start-button').click();
+    cy.get('[data-testid="answer-button"]', { timeout: 8000 }).first().should('be.visible');
+
+    for (let i = 0; i < 10; i++) {
+      cy.get('[data-testid="answer-button"]').first().click();
+    }
+
+    cy.findByTestId('start-button').click();
+    cy.findByTestId('question-text', { timeout: 8000 }).should('be.visible');
+  });
+});
